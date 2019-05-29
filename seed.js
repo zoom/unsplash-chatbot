4e726a398b72b348b0b8b19f0876b61b19f5ad4b
tommygaessler <tommy.gaessler@gmail.com>
2019-05-29 15:32:07 -0600

    debugging stack issue

--- seed.js
+++ seed.js
@@ -4,8 +4,7 @@ const connectionString = process.env.DATABASE_URL;
 const client = new pg.Client(connectionString);
 
 client.connect();
-client.query('CREATE TABLE IF NOT EXISTS table_name (id uuid, url VARCHAR(2000), \
-  title TEXT, description TEXT, been_visited BOOLEAN DEFAULT false, created_at TIMESTAMP DEFAULT NOW())', (err, res) => {
+client.query('CREATE TABLE IF NOT EXISTS table_name (id uuid, url VARCHAR(2000), title TEXT, description TEXT, been_visited BOOLEAN DEFAULT false, created_at TIMESTAMP DEFAULT NOW())', (err, res) => {
     if (err) {
       client.end();
       return console.error('error with PostgreSQL database', err);
