c2814a9573cd63b9f882ceefa6ccd511a8c719ec
tommygaessler <tommy.gaessler@gmail.com>
2019-05-29 15:28:04 -0600

    testing bug

--- seed.js
+++ seed.js
@@ -1,15 +1,30 @@
-require('dotenv').config();
-const { Client } = require('pg');
-const pg = new Client({
-  connectionString: process.env.DATABASE_URL,
-  ssl: process.env.NODE_ENV ? true : false,
+const pg = require('pg');
+const connectionString = process.env.DATABASE_URL;
+
+const client = new pg.Client(connectionString);
+
+client.connect();
+client.query('CREATE TABLE IF NOT EXISTS table_name (id uuid, url VARCHAR(2000), \
+  title TEXT, description TEXT, been_visited BOOLEAN DEFAULT false, created_at TIMESTAMP DEFAULT NOW())', (err, res) => {
+    if (err) {
+      client.end();
+      return console.error('error with PostgreSQL database', err);
+    }
 });
-pg.connect();
-pg.query(`CREATE TABLE access_token(access_token TEXT, expires_on NUMERIC); INSERT INTO access_token (access_token, expires_on) VALUES ('', '1');`, (error, results) => {
-  if(error) {
-    console.log(error);
-  } else {
-    console.log(results);
-  }
-  pg.end();
-})
+
+client.end();
+// require('dotenv').config();
+// const { Client } = require('pg');
+// const pg = new Client({
+//   connectionString: process.env.DATABASE_URL,
+//   ssl: process.env.NODE_ENV ? true : false,
+// });
+// pg.connect();
+// pg.query(`CREATE TABLE access_token(access_token TEXT, expires_on NUMERIC); INSERT INTO access_token (access_token, expires_on) VALUES ('', '1');`, (error, results) => {
+//   if(error) {
+//     console.log(error);
+//   } else {
+//     console.log(results);
+//   }
+//   pg.end();
+// })
