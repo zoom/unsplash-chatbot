71cd020fac70213a543d8054d7402245df5ba206
tommygaessler <tommy.gaessler@gmail.com>
2019-05-30 13:16:45 -0600

    database connection cleanup test

--- app.js
+++ app.js
@@ -1,21 +1,17 @@
-require('dotenv').config();
-const express = require('express');
-const bodyParser = require('body-parser');
-const request = require('request');
+require('dotenv').config()
+const express = require('express')
+const bodyParser = require('body-parser')
+const request = require('request')
 
-const { Client } = require('pg');
+const { Client } = require('pg')
+const pg = new Client(process.env.DATABASE_URL)
 
-const pg = new Client({
-  connectionString: process.env.DATABASE_URL,
-  ssl: process.env.NODE_ENV ? true : false,
-});
-
-pg.connect();
+pg.connect()
 
 const app = express()
 const port = process.env.PORT || 4000
 
-app.use(bodyParser.json());
+app.use(bodyParser.json())
 
 app.get('/', (req, res) => {
   res.send('Welcome to the Unsplash Bot for Zoom Chat!')
@@ -26,13 +22,12 @@ app.get('/authorize', (req, res) => {
 })
 
 app.post('/unsplash', (req, res) => {
-
   pg.query('SELECT * FROM access_token', (error, results) => {
     if (error) {
-      console.log(error);
+      console.log(error)
       res.send('Error in access_token query')
     } else {
-      if(results.rows[0].expires_on > (new Date().getTime() / 1000)) {
+      if (results.rows[0].expires_on > (new Date().getTime() / 1000)) {
         getPhoto(results.rows[0].access_token)
       } else {
         refreshToken()
@@ -40,105 +35,105 @@ app.post('/unsplash', (req, res) => {
     }
   })
 
-  function getPhoto(zoom_access_token) {
+  function getPhoto (zoomAccessToken) {
     request(`https://api.unsplash.com/photos/random?query=${req.body.payload.cmd}&orientation=landscape&client_id=${process.env.unsplash_client_id}`, (error, body) => {
-      if(error) {
-        console.log(error);
+      if (error) {
+        console.log(error)
         res.send('Error in getting photo')
       } else {
         body = JSON.parse(body.body)
-        if(body.errors) {
+        if (body.errors) {
           var errors = [
             {
-              "type": "section",
-              "sidebar_color": "#D72638",
-              "sections": body.errors.map((error) => {
-                return {"type": "message", "text": error}
+              'type': 'section',
+              'sidebar_color': '#D72638',
+              'sections': body.errors.map((error) => {
+                return { 'type': 'message', 'text': error }
               })
             }
           ]
-          sendChat(errors, zoom_access_token)
+          sendChat(errors, zoomAccessToken)
         } else {
           var photo = [
             {
-              "type": "section",
-              "sidebar_color": body.color,
-              "sections": [
+              'type': 'section',
+              'sidebar_color': body.color,
+              'sections': [
                 {
-                  "type": "attachments",
-                  "img_url": body.urls.regular,
-                  "resource_url": body.links.html,
-                  // "ext": "png",
-                  "information": {
-                    "title": {
-                      "text": "Photo by " + body.user.name
+                  'type': 'attachments',
+                  'img_url': body.urls.regular,
+                  'resource_url': body.links.html,
+                  // 'ext': 'png',
+                  'information': {
+                    'title': {
+                      'text': 'Photo by ' + body.user.name
                     },
-                    "description": {
-                      "text": "Click to view on Unsplash"
+                    'description': {
+                      'text': 'Click to view on Unsplash'
                     }
                   }
                 }
               ]
             }
           ]
-          sendChat(photo, zoom_access_token);
+          sendChat(photo, zoomAccessToken)
         }
       }
     })
   }
 
-  function sendChat(chatBody, zoom_access_token) {
-
+  function sendChat (chatBody, zoomAccessToken) {
     request({
-      url:'https://api.zoom.us/v2/im/chat/messages',
+      url: 'https://api.zoom.us/v2/im/chat/messages',
       method: 'POST',
       json: true,
       body: {
-      "robot_jid": process.env.zoom_robot_jid,
-      "to_jid": req.body.payload.toJid,
-      "account_id": req.body.payload.accountId,
-      "content": {
-        "head": {
-          "text": "/unsplash " + req.body.payload.cmd
-        },
-        "body": chatBody
+        'robot_jid': process.env.zoom_robot_jid,
+        'to_jid': req.body.payload.toJid,
+        'account_id': req.body.payload.accountId,
+        'content': {
+          'head': {
+            'text': '/unsplash ' + req.body.payload.cmd
+          },
+          'body': chatBody
+        }
+      },
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer ' + zoomAccessToken
       }
-    },
-    headers: {
-      "Content-Type": "application/json",
-      "Authorization": "Bearer " + zoom_access_token
-    }}, (error, httpResponse, body) => {
-      console.log(body);
+    }, (error, httpResponse, body) => {
+      console.log(body)
       if (error) {
         res.send('Error sending chat')
-        console.log(error);
+        console.log(error)
       } else {
         res.send('Chat sent successfully')
       }
-    });
+    })
   }
 
-  function refreshToken() {
+  function refreshToken () {
     request({
-      url:`https://api.zoom.us/oauth/token?grant_type=client_credentials&client_id=${process.env.zoom_client_id}&client_secret=${process.env.zoom_client_secret}`,
+      url: `https://api.zoom.us/oauth/token?grant_type=client_credentials&client_id=${process.env.zoom_client_id}&client_secret=${process.env.zoom_client_secret}`,
       method: 'POST'
     }, (error, httpResponse, body) => {
       if (error) {
-        console.log(error);
+        console.log(error)
         res.send('Error refreshing token')
       } else {
-        body = JSON.parse(body);
+        body = JSON.parse(body)
 
         pg.query(`UPDATE access_token SET access_token = '${body.access_token}', expires_on = ${(new Date().getTime() / 1000) + body.expires_in}`, (error, results) => {
           if (error) {
-            console.log(error);
+            console.log(error)
             res.send('Error setting access_token')
           } else {
             getPhoto(body.access_token)
           }
         })
       }
-    });
+    })
   }
 })
 
@@ -158,4 +153,4 @@ app.post('/deauthorize', (req, res) => {
   res.send('deauthorized')
 })
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.listen(port, () => console.log(`Chatbot listening on port ${port}!`))
--- seed.js
+++ seed.js
@@ -1,15 +1,15 @@
-require('dotenv').config();
-const { Client } = require('pg');
-const pg = new Client({
-  connectionString: process.env.DATABASE_URL,
-  ssl: process.env.NODE_ENV ? true : false,
-});
-pg.connect();
+require('dotenv').config()
+
+const { Client } = require('pg')
+const pg = new Client(process.env.DATABASE_URL)
+
+pg.connect()
+
 pg.query(`CREATE TABLE access_token(access_token TEXT, expires_on NUMERIC); INSERT INTO access_token (access_token, expires_on) VALUES ('', '1');`, (error, results) => {
-  if(error) {
-    console.log(error);
+  if (error) {
+    console.log(error)
   } else {
-    console.log(results);
+    console.log(results)
   }
-  pg.end();
+  pg.end()
 })
