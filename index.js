ccca3d523bb5e61faa2361d2747a383816d5cb03
Tommy Gaessler <tommy.gaessler@zoom.us>
2024-12-10 10:29:39 -0700

    simplified logic

--- index.js
+++ index.js
@@ -2,8 +2,9 @@ require('dotenv').config()
 const express = require('express')
 const bodyParser = require('body-parser')
 const axios = require('axios')
-const crypto = require('crypto')
 
+// database
+const crypto = require('crypto')
 const { Client } = require('pg')
 const pg = new Client({
   connectionString:  process.env.DATABASE_URL,
@@ -17,13 +18,13 @@ pg.connect().catch((error) => {
   console.log('Error connecting to database', error)
 })
 
+// server
 const app = express()
 const port = process.env.PORT || 4000
 
 app.use(bodyParser.json())
 
 // routes:
-
 app.get('/', (req, res) => {
   res.send('Welcome to Unsplash for Zoom Team Chat!')
 })
@@ -52,63 +53,61 @@ app.post('/unsplash', (req, res) => {
 
   var response
 
+  if (verifyWebhook(req)) {
+
+    response = { message: 'Authorized request to Unsplash for Zoom Team Chat.', status: 200 }
+
+    console.log(response.message)
+
+    if (req.body.event === 'endpoint.url_validation') {
+      response = {
+        message: {
+          plainToken: req.body.payload.plainToken,
+          encryptedToken: validateWebhook(req)
+        },
+        status: 200
+      }
+    } else if(req.body.event === 'bot_notification') {
+      console.log('Zoom Team Chat App message received.')
+      getUser(req.body.payload)
+    } else if(req.body.event === 'bot_installed') {
+      console.log('Unsplash for Zoom Team Chat installed.')
+    } else if(req.body.event === 'app_deauthorized') {
+      console.log('Unsplash for Zoom Team Chat uninstalled.')
+      deleteUser(req.body.payload)
+    } else {
+      console.log(req.body)
+      console.log('Unsupported Zoom webhook event type: ', req.body.event)
+    }
+
+    console.log(response.message)
+    res.status(response.status)
+    res.json(response)
+  } else {
+    response = { message: 'Unauthorized request to Unsplash for Zoom Team Chat.', status: 401 }
+
+    console.log(response.message)
+    res.status(response.status)
+    res.json(response)
+  }
+})
+
+app.listen(port, () => console.log(`Unsplash for Zoom Team Chat listening on port ${port}!`))
+
+// helper functions:
+function verifyWebhook(req) {
   const message = `v0:${req.headers['x-zm-request-timestamp']}:${JSON.stringify(req.body)}`
 
   const hashForVerify = crypto.createHmac('sha256', process.env.ZOOM_WEBHOOK_SECRET_TOKEN).update(message).digest('hex')
 
   const signature = `v0=${hashForVerify}`
 
-  if (req.headers['x-zm-signature'] === signature) {
+  return req.headers['x-zm-signature'] === signature
+}
 
-    if(req.body.event === 'endpoint.url_validation') {
-
-      const hashForValidate = crypto.createHmac('sha256', process.env.ZOOM_WEBHOOK_SECRET_TOKEN).update(req.body.payload.plainToken).digest('hex')
-
-      response = {
-        message: {
-          plainToken: req.body.payload.plainToken,
-          encryptedToken: hashForValidate
-        },
-        status: 200
-      }
-
-      console.log(response.message)
-
-      res.status(response.status)
-      res.json(response.message)
-    } else {
-
-      response = { message: 'Authorized request to Unsplash for Zoom Team Chat.', status: 200 }
-
-      console.log(response.message)
-
-      res.status(response.status)
-      res.json(response)
-
-      if(req.body.event === 'bot_notification') {
-        console.log('Zoom Team Chat App message received.')
-        getUser(req.body.payload)
-      } else if(req.body.event === 'bot_installed') {
-        console.log('Unsplash for Zoom Team Chat installed.')
-      } else if(req.body.event === 'app_deauthorized') {
-        console.log('Unsplash for Zoom Team Chat uninstalled.')
-        deleteUser(req.body.payload)
-      } else {
-        console.log('Unsupported Zoom webhook event type: ', req.body.event)
-        console.log(req.body)
-      }
-    }
-  } else {
-    response = { message: 'Unauthorized request to Unsplash for Zoom Team Chat.', status: 401 }
-
-    console.log(response.message)
-
-    res.status(response.status)
-    res.json(response)
-  }
-})
-
-// helper functions:
+function validateWebhook(req) {
+  return crypto.createHmac('sha256', process.env.ZOOM_WEBHOOK_SECRET_TOKEN).update(req.body.payload.plainToken).digest('hex')
+}
 
 function getUser(payload) {
   pg.query(`SELECT PGP_SYM_DECRYPT(access_token::bytea, '${process.env.DATABASE_ENCRYPTION_KEY}') as access_token, PGP_SYM_DECRYPT(expires_on::bytea, '${process.env.DATABASE_ENCRYPTION_KEY}') as expires_on FROM users WHERE PGP_SYM_DECRYPT(account_id::bytea, '${process.env.DATABASE_ENCRYPTION_KEY}') = '${payload.accountId}';`, (error, results) => {
@@ -122,7 +121,7 @@ function getUser(payload) {
         if(results.rows[0].expires_on > (new Date().getTime() / 1000)) {
           console.log('User access token active.')
 
-          getPhoto(results.rows[0].access_token, payload)
+          getPhoto(payload, results.rows[0].access_token)
         } else {
           console.log('User access token expired.')
 
@@ -137,17 +136,26 @@ function getUser(payload) {
   })
 }
 
-function deleteUser(payload) {
-  pg.query(`DELETE FROM users WHERE PGP_SYM_DECRYPT(account_id::bytea,'${process.env.DATABASE_ENCRYPTION_KEY}') = '${payload.account_id}';`, (error, results) => {
-    if (error) {
-      console.log('Error removing user from database.', error)
-    } else {
-      console.log('Successfully removed user from database.')
+function getAccessToken (payload, accountExists) {
+  axios.post('https://api.zoom.us/oauth/token?grant_type=client_credentials', {}, {
+    headers: {
+      'Authorization': 'Basic ' + Buffer.from(process.env.ZOOM_CLIENT_ID + ':' + process.env.ZOOM_CLIENT_SECRET).toString('base64')
     }
+  }).then((body) => {
+
+    console.log('Successfully received Team Chat App token from Zoom.')
+
+    if(accountExists) {
+      updateUser(payload, body)
+    } else {
+      addUser(payload, body)
+    }
+  }).catch((error) => {
+    console.log('Error getting Team Chat App token from Zoom.', error)
   })
 }
 
-function getPhoto (accessToken, payload) {
+function getPhoto (payload, accessToken) {
   axios.get(`https://api.unsplash.com/photos/random?query=${payload.cmd}&orientation=landscape&client_id=${process.env.UNSPLASH_ACCESS_KEY}`).then((body) => {
 
     console.log('Successfully received photo from Unsplash.')
@@ -173,7 +181,7 @@ function getPhoto (accessToken, payload) {
         ]
       }
     ]
-    sendChat(photoBody, accessToken, payload)
+    sendChat(payload, accessToken, photoBody)
 
   }).catch((error) => {
     console.log('Error getting photo from Unsplash.', error.response.data.errors)
@@ -187,12 +195,11 @@ function getPhoto (accessToken, payload) {
         })
       }
     ]
-    sendChat(errorsBody, accessToken, payload)
+    sendChat(payload, accessToken, errorsBody)
   })
 }
 
-function sendChat (messageBody, accessToken, payload) {
-
+function sendChat (payload, accessToken, messageBody) {
   axios.post('https://api.zoom.us/v2/im/chat/messages', {
     'robot_jid': process.env.ZOOM_BOT_JID,
     'to_jid': payload.toJid,
@@ -218,40 +225,36 @@ function sendChat (messageBody, accessToken, payload) {
   })
 }
 
-function getAccessToken (payload, accountExists) {
-
-  axios.post('https://api.zoom.us/oauth/token?grant_type=client_credentials', {}, {
-    headers: {
-      'Authorization': 'Basic ' + Buffer.from(process.env.ZOOM_CLIENT_ID + ':' + process.env.ZOOM_CLIENT_SECRET).toString('base64')
-    }
-  }).then((body) => {
-
-    console.log('Successfully received Team Chat App token from Zoom.')
-
-    if(accountExists) {
-      pg.query(`UPDATE users SET (access_token, expires_on) = (PGP_SYM_ENCRYPT('${body.data.access_token}', '${process.env.DATABASE_ENCRYPTION_KEY}', 'cipher-algo=${process.env.DATABASE_ENCRYPTION_ALGO}'), PGP_SYM_ENCRYPT('${(new Date().getTime() / 1000) + body.data.expires_in}', '${process.env.DATABASE_ENCRYPTION_KEY}', 'cipher-algo=${process.env.DATABASE_ENCRYPTION_ALGO}')) WHERE PGP_SYM_DECRYPT(account_id::bytea, '${process.env.DATABASE_ENCRYPTION_KEY}') = '${payload.accountId}';`, (error, results) => {
-        if (error) {
-          console.log('Error updating user in database.', error)
-        } else {
-          console.log('Successfully updated user in database.')
-
-          getPhoto(body.data.access_token, payload)
-        }
-      })
+function addUser(payload, body) {
+  pg.query(`INSERT INTO users (account_id, access_token, expires_on) VALUES (PGP_SYM_ENCRYPT('${payload.accountId}', '${process.env.DATABASE_ENCRYPTION_KEY}', 'cipher-algo=${process.env.DATABASE_ENCRYPTION_ALGO}'), PGP_SYM_ENCRYPT('${body.data.access_token}', '${process.env.DATABASE_ENCRYPTION_KEY}', 'cipher-algo=${process.env.DATABASE_ENCRYPTION_ALGO}'), PGP_SYM_ENCRYPT('${(new Date().getTime() / 1000) + body.data.expires_in}', '${process.env.DATABASE_ENCRYPTION_KEY}', 'cipher-algo=${process.env.DATABASE_ENCRYPTION_ALGO}'));`, (error, results) => {
+    if (error) {
+      console.log('Error inserting user in database.', error)
     } else {
-      pg.query(`INSERT INTO users (account_id, access_token, expires_on) VALUES (PGP_SYM_ENCRYPT('${payload.accountId}', '${process.env.DATABASE_ENCRYPTION_KEY}', 'cipher-algo=${process.env.DATABASE_ENCRYPTION_ALGO}'), PGP_SYM_ENCRYPT('${body.data.access_token}', '${process.env.DATABASE_ENCRYPTION_KEY}', 'cipher-algo=${process.env.DATABASE_ENCRYPTION_ALGO}'), PGP_SYM_ENCRYPT('${(new Date().getTime() / 1000) + body.data.expires_in}', '${process.env.DATABASE_ENCRYPTION_KEY}', 'cipher-algo=${process.env.DATABASE_ENCRYPTION_ALGO}'));`, (error, results) => {
-        if (error) {
-          console.log('Error inserting user in database.', error)
-        } else {
-          console.log('Successfully inserted user in database.')
+      console.log('Successfully inserted user in database.')
 
-          getPhoto(body.data.access_token, payload)
-        }
-      })
+      getPhoto(payload, body.data.access_token)
     }
-  }).catch((error) => {
-    console.log('Error getting Team Chat App token from Zoom.', error)
   })
 }
 
-app.listen(port, () => console.log(`Unsplash for Zoom Team Chat listening on port ${port}!`))
+function updateUser(payload, body) {
+  pg.query(`UPDATE users SET (access_token, expires_on) = (PGP_SYM_ENCRYPT('${body.data.access_token}', '${process.env.DATABASE_ENCRYPTION_KEY}', 'cipher-algo=${process.env.DATABASE_ENCRYPTION_ALGO}'), PGP_SYM_ENCRYPT('${(new Date().getTime() / 1000) + body.data.expires_in}', '${process.env.DATABASE_ENCRYPTION_KEY}', 'cipher-algo=${process.env.DATABASE_ENCRYPTION_ALGO}')) WHERE PGP_SYM_DECRYPT(account_id::bytea, '${process.env.DATABASE_ENCRYPTION_KEY}') = '${payload.accountId}';`, (error, results) => {
+    if (error) {
+      console.log('Error updating user in database.', error)
+    } else {
+      console.log('Successfully updated user in database.')
+
+      getPhoto(payload, body.data.access_token)
+    }
+  })
+}
+
+function deleteUser(payload) {
+  pg.query(`DELETE FROM users WHERE PGP_SYM_DECRYPT(account_id::bytea,'${process.env.DATABASE_ENCRYPTION_KEY}') = '${payload.account_id}';`, (error, results) => {
+    if (error) {
+      console.log('Error removing user from database.', error)
+    } else {
+      console.log('Successfully removed user from database.')
+    }
+  })
+}
